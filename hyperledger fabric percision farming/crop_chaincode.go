1217dbe98a88ccd615ff4e2550a18e60c1360068
manilpuri9 <manilpuri9@gmail.com>
2018-03-06 00:03:34 +0800

    crop chaincode fully developed with all function, successfully built but not tested on network

--- hyperledger fabric percision farming/crop_chaincode.go
+++ hyperledger fabric percision farming/crop_chaincode.go
@@ -80,18 +80,18 @@ type SoilConditionType struct {
 }
 
 type Crop struct {
-	Name               string            `json:"name"`
-	Owner              string            `json:"owner"`
-	Quantity           int               `json:"quantity"`
-	FarmInfo           FarmInfoType      `json:"farm_info"`
-	Weather            WeatherType       `json:"weather"`
-	SoilCondition      SoilConditionType `json:"soil_condition"`
-	Image              string            `json:"image"`
-	Cghc               int               `json:"cghc"`
-	Irrigation         bool              `json:"irrigation"`
-	FertilizerAddition bool              `json:"fertilizer_addition"`
-	ApplyPesticide     bool              `json:"apply_pesticide"`
-	Harvesting         bool              `json:"harvesting"`
+	Name           string            `json:"name"`
+	Owner          string            `json:"owner"`
+	Quantity       int               `json:"quantity"`
+	FarmInfo       FarmInfoType      `json:"farm_info"`
+	Weather        WeatherType       `json:"weather"`
+	SoilCondition  SoilConditionType `json:"soil_condition"`
+	Image          string            `json:"image"`
+	Cghc           int               `json:"cghc"`
+	Irrigation     bool              `json:"irrigation"`
+	AddFertilizer  bool              `json:"fertilizer_addition"`
+	ApplyPesticide bool              `json:"apply_pesticide"`
+	Harvesting     bool              `json:"harvesting"`
 }
 
 // ===================================================================================
@@ -121,41 +121,39 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return t.initCrop(stub, args)
 	} else if function == "queryCrop" { //find Crop based on an ad hoc rich query
 		return t.queryCrop(stub, args)
+	} else if function == "historyOfCrop" { //find Crop based on an ad hoc rich query
+		return t.getHistoryForCrop(stub, args)
+	} else if function == "readCrop" { //find Crop based on an ad hoc rich query
+		return t.readCrop(stub, args)
+	} else if function == "deleteCrop" { //find Crop based on an ad hoc rich query
+		return t.delete(stub, args)
+	} else if function == "irrigationCrop" { //find Crop based on an ad hoc rich query
+		return t.irrigation(stub, args)
+	} else if function == "addFertilizerCrop" { //find Crop based on an ad hoc rich query
+		return t.addFertilizer(stub, args)
+	} else if function == "applyPesticideCrop" { //find Crop based on an ad hoc rich query
+		return t.applyPesticide(stub, args)
+	} else if function == "harvestCrop" { //find Crop based on an ad hoc rich query
+		return t.harvest(stub, args)
 	}
 
-	// else if function == "readMarble" { //read a Crop
-	// 	return t.readCrop(stub, args)
-	// }
 	fmt.Println("invoke did not find func: " + function) //error
 	return shim.Error("Received unknown function invocation")
 }
 
 // ============================================================
-// initMarble - create a new marble, store into chaincode state
+// initMarble - create a new Crop, store into chaincode state
 // ============================================================
 func (t *SimpleChaincode) initCrop(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var err error
 
-	//   0       1       2     3
-	// "asdf", "blue", "35", "bob"
 	if len(args) != 20 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
+		return shim.Error("Incorrect number of arguments. Expecting 20")
 	}
 
 	// ==== Input sanitation ====
 	fmt.Println("- start init crop")
-	// if len(args[0]) <= 0 {
-	// 	return shim.Error("1st argument must be a non-empty string")
-	// }
-	// if len(args[1]) <= 0 {
-	// 	return shim.Error("2nd argument must be a non-empty string")
-	// }
-	// if len(args[2]) <= 0 {
-	// 	return shim.Error("3rd argument must be a non-empty string")
-	// }
-	// if len(args[3]) <= 0 {
-	// 	return shim.Error("4th argument must be a non-empty string")
-	// }
+
 	cropnamev := args[0]
 	ownerv := args[1]
 	quantityv, err := strconv.Atoi(args[2])
@@ -217,12 +215,12 @@ func (t *SimpleChaincode) initCrop(stub shim.ChaincodeStubInterface, args []stri
 				Percentage: phosv,
 			},
 		},
-		Image:              imagev,
-		Cghc:               cgphv,
-		Irrigation:         irrv,
-		FertilizerAddition: ferv,
-		ApplyPesticide:     appv,
-		Harvesting:         harv,
+		Image:          imagev,
+		Cghc:           cgphv,
+		Irrigation:     irrv,
+		AddFertilizer:  ferv,
+		ApplyPesticide: appv,
+		Harvesting:     harv,
 	}
 
 	// ==== Check if crop already exists ====
@@ -268,6 +266,123 @@ func (t *SimpleChaincode) initCrop(stub shim.ChaincodeStubInterface, args []stri
 
 	// ==== Crop saved and indexed. Return success ====
 	fmt.Println("- end init crop successful")
+	return shim.Success(nil)
+}
+
+// ============================================================
+// UpdateCrop - updates info of Crop, store into chaincode state
+// ============================================================
+func (t *SimpleChaincode) updateCrop(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var err error
+	var cropJSON Crop
+
+	if len(args) != 16 {
+		return shim.Error("Incorrect number of arguments. Expecting 20")
+	}
+
+	// ==== Input sanitation ====
+	fmt.Println("- start update crop")
+
+	cropnamev := args[0]
+
+	// ==== Check if crop already exists ====
+	gotCropAsBytes, err := stub.GetState(cropnamev)
+	if err != nil {
+		return shim.Error("Failed to get marble: " + err.Error())
+	}
+	err = json.Unmarshal([]byte(gotCropAsBytes), &cropJSON)
+	if err != nil {
+		return shim.Error("Failed to unmarshal crop to json format " + err.Error())
+	}
+
+	ownerv := cropJSON.Owner
+	quantityv := cropJSON.Quantity
+	lativ := cropJSON.FarmInfo.GeoLocation.Latitude
+	longiv := cropJSON.FarmInfo.GeoLocation.Longitude
+	soilv := cropJSON.FarmInfo.SoilType
+
+	celv, err := strconv.ParseFloat(args[6], 64)
+	pasv, err := strconv.ParseFloat(args[7], 64)
+	humv, err := strconv.ParseFloat(args[8], 64)
+	radv, err := strconv.ParseFloat(args[9], 64)
+	moistv, err := strconv.ParseFloat(args[10], 64)
+	phv, err := strconv.Atoi(args[11])
+	nitrov, err := strconv.ParseFloat(args[12], 64)
+	phosv, err := strconv.ParseFloat(args[13], 64)
+	imagev := args[14]
+	cgphv, err := strconv.Atoi(args[15])
+	irrv := cropJSON.Irrigation
+	ferv := cropJSON.AddFertilizer
+	appv := cropJSON.ApplyPesticide
+	harv := cropJSON.Harvesting
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	crop := Crop{
+		Name:     cropnamev,
+		Owner:    ownerv,
+		Quantity: quantityv,
+		FarmInfo: FarmInfoType{
+			GeoLocation: GeoLocationType{
+				Latitude:  lativ,
+				Longitude: longiv,
+			},
+			SoilType: strings.ToLower(soilv),
+		},
+		Weather: WeatherType{
+			Temperature: TemperatureType{
+				Celcius: celv,
+			},
+			Pressure: PressureType{
+				Pascal: pasv,
+			},
+			Humidity: HumidityType{
+				CubicMeter: humv,
+			},
+			Radiation: RadiationType{
+				Rem: radv,
+			},
+		},
+		//soil condition
+		SoilCondition: SoilConditionType{
+			Moisture: MoistureType{
+				CubicMeter: moistv,
+			},
+			Ph: phv,
+			Nitrogen: NitrogenType{
+				Percentage: nitrov,
+			},
+			Phosphorus: PhosphorusType{
+				Percentage: phosv,
+			},
+		},
+		Image:          imagev,
+		Cghc:           cgphv,
+		Irrigation:     irrv,
+		AddFertilizer:  ferv,
+		ApplyPesticide: appv,
+		Harvesting:     harv,
+	}
+
+	// ==== Create crop object and crop to JSON ====
+	cropJsonBytes, err := json.Marshal(crop)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	//Alternatively, build the marble json string manually if you don't want to use struct marshalling
+	//marbleJSONasString := `{"docType":"Marble",  "name": "` + marbleName + `", "color": "` + color + `", "size": ` + strconv.Itoa(size) + `, "owner": "` + owner + `"}`
+	//marbleJSONasBytes := []byte(str)
+
+	// === Save marble to state ===
+	err = stub.PutState(cropnamev, cropJsonBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ==== Crop update done Return success ====
+	fmt.Println("- end update crop successful")
 	return shim.Success(nil)
 }
 
@@ -430,42 +545,42 @@ func (t *SimpleChaincode) readCrop(stub shim.ChaincodeStubInterface, args []stri
 // ==================================================
 func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var jsonResp string
-	var marbleJSON marble
+	var cropJSON Crop
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
-	marbleName := args[0]
+	cropName := args[0]
 
-	// to maintain the color~name index, we need to read the marble first and get its color
-	valAsbytes, err := stub.GetState(marbleName) //get the marble from chaincode state
+	// to maintain the owner~name index, we need to read the crop first and get its owner
+	valAsbytes, err := stub.GetState(cropName) //get the crop from chaincode state
 	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + marbleName + "\"}"
+		jsonResp = "{\"Error\":\"Failed to get state for " + cropName + "\"}"
 		return shim.Error(jsonResp)
 	} else if valAsbytes == nil {
-		jsonResp = "{\"Error\":\"Marble does not exist: " + marbleName + "\"}"
+		jsonResp = "{\"Error\":\"crop does not exist: " + cropName + "\"}"
 		return shim.Error(jsonResp)
 	}
 
-	err = json.Unmarshal([]byte(valAsbytes), &marbleJSON)
+	err = json.Unmarshal([]byte(valAsbytes), &cropJSON)
 	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to decode JSON of: " + marbleName + "\"}"
+		jsonResp = "{\"Error\":\"Failed to decode JSON of: " + cropName + "\"}"
 		return shim.Error(jsonResp)
 	}
 
-	err = stub.DelState(marbleName) //remove the marble from chaincode state
+	err = stub.DelState(cropName) //remove the crop from chaincode state
 	if err != nil {
 		return shim.Error("Failed to delete state:" + err.Error())
 	}
 
 	// maintain the index
-	indexName := "color~name"
-	colorNameIndexKey, err := stub.CreateCompositeKey(indexName, []string{marbleJSON.Color, marbleJSON.Name})
+	indexName := "owner~name"
+	ownerNameIndexKey, err := stub.CreateCompositeKey(indexName, []string{cropJSON.Owner, cropJSON.Name})
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
 	//  Delete index entry to state.
-	err = stub.DelState(colorNameIndexKey)
+	err = stub.DelState(ownerNameIndexKey)
 	if err != nil {
 		return shim.Error("Failed to delete state:" + err.Error())
 	}
@@ -475,155 +590,159 @@ func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string
 // ===========================================================
 // irrigation
 // ===========================================================
-func (t *SimpleChaincode) transferMarble(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) irrigation(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	//   0       1
-	// "name", "bob"
 	if len(args) < 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
-	marbleName := args[0]
-	newOwner := strings.ToLower(args[1])
-	fmt.Println("- start transferMarble ", marbleName, newOwner)
-
-	marbleAsBytes, err := stub.GetState(marbleName)
+	cropName := args[0]
+	newIrrigationValue, err := strconv.ParseBool(args[1])
 	if err != nil {
-		return shim.Error("Failed to get marble:" + err.Error())
-	} else if marbleAsBytes == nil {
-		return shim.Error("Marble does not exist")
+		return shim.Error("Unable to parse boolean")
+	}
+	fmt.Println("- start irrigation value update", cropName, newIrrigationValue)
+
+	cropAsBytes, err := stub.GetState(cropName)
+	if err != nil {
+		return shim.Error("Failed to get crop:" + err.Error())
+	} else if cropAsBytes == nil {
+		return shim.Error("crop does not exist")
 	}
 
-	marbleToTransfer := marble{}
-	err = json.Unmarshal(marbleAsBytes, &marbleToTransfer) //unmarshal it aka JSON.parse()
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	marbleToTransfer.Owner = newOwner //change the owner
-
-	marbleJSONasBytes, _ := json.Marshal(marbleToTransfer)
-	err = stub.PutState(marbleName, marbleJSONasBytes) //rewrite the marble
+	cropIrrigation := Crop{}
+	err = json.Unmarshal(cropAsBytes, &cropIrrigation) //unmarshal it aka JSON.parse()
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	cropIrrigation.Irrigation = newIrrigationValue //change the owner
 
-	fmt.Println("- end transferMarble (success)")
+	cropJSONasBytes, _ := json.Marshal(cropIrrigation)
+	err = stub.PutState(cropName, cropJSONasBytes) //rewrite the marble
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("- end irrigation value update(successful)")
 	return shim.Success(nil)
 }
 
 // ===========================================================
 // add fertilizer
 // ===========================================================
-func (t *SimpleChaincode) transferMarble(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) addFertilizer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	//   0       1
-	// "name", "bob"
 	if len(args) < 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
-	marbleName := args[0]
-	newOwner := strings.ToLower(args[1])
-	fmt.Println("- start transferMarble ", marbleName, newOwner)
-
-	marbleAsBytes, err := stub.GetState(marbleName)
+	cropName := args[0]
+	newFertilizerValue, err := strconv.ParseBool(args[1])
 	if err != nil {
-		return shim.Error("Failed to get marble:" + err.Error())
-	} else if marbleAsBytes == nil {
-		return shim.Error("Marble does not exist")
+		return shim.Error("Unable to parse boolean")
+	}
+	fmt.Println("- start fertilization value update ", cropName, newFertilizerValue)
+
+	cropAsBytes, err := stub.GetState(cropName)
+	if err != nil {
+		return shim.Error("Failed to get crop:" + err.Error())
+	} else if cropAsBytes == nil {
+		return shim.Error("crop does not exist")
 	}
 
-	marbleToTransfer := marble{}
-	err = json.Unmarshal(marbleAsBytes, &marbleToTransfer) //unmarshal it aka JSON.parse()
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	marbleToTransfer.Owner = newOwner //change the owner
-
-	marbleJSONasBytes, _ := json.Marshal(marbleToTransfer)
-	err = stub.PutState(marbleName, marbleJSONasBytes) //rewrite the marble
+	cropFertilization := Crop{}
+	err = json.Unmarshal(cropAsBytes, &cropFertilization) //unmarshal it aka JSON.parse()
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	cropFertilization.AddFertilizer = newFertilizerValue //change the fertilizer value
 
-	fmt.Println("- end transferMarble (success)")
+	cropJSONasBytes, _ := json.Marshal(cropFertilization)
+	err = stub.PutState(cropName, cropJSONasBytes) //rewrite the crop
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("- end addFertilizer value update(successful)")
 	return shim.Success(nil)
 }
 
 // ===========================================================
 // Add pesticide
 // ===========================================================
-func (t *SimpleChaincode) transferMarble(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) applyPesticide(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	//   0       1
-	// "name", "bob"
 	if len(args) < 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
-	marbleName := args[0]
-	newOwner := strings.ToLower(args[1])
-	fmt.Println("- start transferMarble ", marbleName, newOwner)
-
-	marbleAsBytes, err := stub.GetState(marbleName)
+	cropName := args[0]
+	newPesticideValue, err := strconv.ParseBool(args[1])
 	if err != nil {
-		return shim.Error("Failed to get marble:" + err.Error())
-	} else if marbleAsBytes == nil {
-		return shim.Error("Marble does not exist")
+		return shim.Error("Unable to parse boolean")
+	}
+	fmt.Println("- start applyPesticide value update ", cropName, newPesticideValue)
+
+	cropAsBytes, err := stub.GetState(cropName)
+	if err != nil {
+		return shim.Error("Failed to get crop:" + err.Error())
+	} else if cropAsBytes == nil {
+		return shim.Error("crop does not exist")
 	}
 
-	marbleToTransfer := marble{}
-	err = json.Unmarshal(marbleAsBytes, &marbleToTransfer) //unmarshal it aka JSON.parse()
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	marbleToTransfer.Owner = newOwner //change the owner
-
-	marbleJSONasBytes, _ := json.Marshal(marbleToTransfer)
-	err = stub.PutState(marbleName, marbleJSONasBytes) //rewrite the marble
+	cropPesticideAddition := Crop{}
+	err = json.Unmarshal(cropAsBytes, &cropPesticideAddition) //unmarshal it aka JSON.parse()
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	cropPesticideAddition.ApplyPesticide = newPesticideValue //change the ApplyPesticide value
 
-	fmt.Println("- end transferMarble (success)")
+	cropJSONasBytes, _ := json.Marshal(cropPesticideAddition)
+	err = stub.PutState(cropName, cropJSONasBytes) //rewrite the crop
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("- end applyPesticide value update(successful)")
 	return shim.Success(nil)
 }
 
 // ===========================================================
 // Harvesting
 // ===========================================================
-func (t *SimpleChaincode) transferMarble(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) harvest(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	//   0       1
-	// "name", "bob"
 	if len(args) < 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
-	marbleName := args[0]
-	newOwner := strings.ToLower(args[1])
-	fmt.Println("- start transferMarble ", marbleName, newOwner)
-
-	marbleAsBytes, err := stub.GetState(marbleName)
+	cropName := args[0]
+	newHarvestValue, err := strconv.ParseBool(args[1])
 	if err != nil {
-		return shim.Error("Failed to get marble:" + err.Error())
-	} else if marbleAsBytes == nil {
-		return shim.Error("Marble does not exist")
+		return shim.Error("Unable to parse boolean")
+	}
+	fmt.Println("- start harvest value update ", cropName, newHarvestValue)
+
+	cropAsBytes, err := stub.GetState(cropName)
+	if err != nil {
+		return shim.Error("Failed to get crop:" + err.Error())
+	} else if cropAsBytes == nil {
+		return shim.Error("crop does not exist")
 	}
 
-	marbleToTransfer := marble{}
-	err = json.Unmarshal(marbleAsBytes, &marbleToTransfer) //unmarshal it aka JSON.parse()
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	marbleToTransfer.Owner = newOwner //change the owner
-
-	marbleJSONasBytes, _ := json.Marshal(marbleToTransfer)
-	err = stub.PutState(marbleName, marbleJSONasBytes) //rewrite the marble
+	cropHarvest := Crop{}
+	err = json.Unmarshal(cropAsBytes, &cropHarvest) //unmarshal it aka JSON.parse()
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	cropHarvest.Harvesting = newHarvestValue //change the harvest value
 
-	fmt.Println("- end transferMarble (success)")
+	cropJSONasBytes, _ := json.Marshal(cropHarvest)
+	err = stub.PutState(cropName, cropJSONasBytes) //rewrite the crop
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("- end Harvest value update(successful)")
 	return shim.Success(nil)
 }
